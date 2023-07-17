32c4bf55f16f8f27c2ac1bc51d0d2b1ba377cd7b
Dennis Anggara <dnnsanggara@gmail.com>
2023-07-17 11:37:02 +0700

    added register route

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const crypto = require('crypto');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 
@@ -7,8 +8,8 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
-app.post('/', (req, res) => {
-    console.log('Server Hit!')
+app.post('/login', (req, res) => {
+    console.log('Server Hit! (login route)');
     const {username, password} = req.body;
     if(username === "dnns" && password === "anggara"){
         res.status(200).send('Login Successful');
@@ -16,6 +17,18 @@ app.post('/', (req, res) => {
     else {
         res.status(401).send('Invalid Credentials');
     }
+})
+
+app.post('/register', (req, res) => {
+    console.log('Server Hit! (register route)')
+    const {username, password, email} = req.body;
+    const id = crypto.randomBytes(16).toString('hex');
+    res.status(201).send({
+        message: "User Created Successfully",
+        userId: id,
+        username: username,
+        email: email
+    });
 })
 
 const PORT = 8000;
